548de067cb13d63dc9183a3dcb01a24e5f30a7a4
Pearl Wong <pwong09@gmail.com>
2022-11-29 15:51:29 -0800

    Tie in GeoCities API and implement search label

---
+++ src/components/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '48a760ae89mshb291042fb63ecadp1a452cjsn8f6afdce714b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,14 +1,31 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../api.js";
 
 // using async paginate
 const Search = ({onSearchChange}) => {
 
   const [search, setSearch] = useState(null);
 
+  const loadOptions = (inputValue) => {
+    return fetch(`${GEO_API_URL}/cities?namePrefix=${inputValue}`, geoApiOptions)
+	          .then(response => response.json())
+	          .then(response => {
+              return {
+                options: response.data.map((city) => {
+                  return {
+                    value: `${city.latitude} ${city.longitude}`,
+                    label: `${city.name}, ${city.countryCode}`
+                  }
+                })
+              }
+            })
+	          .catch(err => console.error(err));
+  }
+
   const handleOnChange = (searchData) => {
     setSearch(searchData);
-    onSearchChange();
+    onSearchChange(searchData);
   }
 
   return (
@@ -17,6 +34,7 @@ const Search = ({onSearchChange}) => {
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
   );
 }
